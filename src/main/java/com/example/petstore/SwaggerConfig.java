72076f0567edaa4d542c33b8538d2de055f11cd1
supriya-nandana <nandanasupriya98@gmail.com>
2020-07-07 11:29:34 +0530

    started pet store application

---
+++ src/main/java/com/example/petstore/SwaggerConfig.java
@@ -0,0 +1,33 @@
+package com.example.petstore;
+
+import java.util.Collections;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import com.google.common.base.Predicates;
+
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.Contact;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+@Configuration
+@EnableSwagger2
+public class SwaggerConfig {
+
+	@Bean
+	public Docket api() {
+		return new Docket(DocumentationType.SWAGGER_2).select().apis(RequestHandlerSelectors.any())
+				.paths(Predicates.not(PathSelectors.regex("/error.*"))).build().apiInfo(apiInfo());
+	}
+
+
+	private ApiInfo apiInfo() {            
+    	return new ApiInfo("Pet Store", "pet Store ", "1", "terms",                  
+    			new Contact("supriya", "www.petstore.com", "support@petstore.com"), "license number", "lecence url", Collections.emptyList());     
+    }
+}
